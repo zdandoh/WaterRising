0f416d1a2f456b512ff46fa3da0c3544b40f0778
Dan <link.dohi@gmail.com>
2013-11-20 10:54:30 -0800

    Work on command and world interaction

--- World.cs
+++ World.cs
@@ -12,16 +12,11 @@ namespace WaterRising
         public static byte[] GetSurround(int[] loc)
         {
             byte[] surroundings = { 0, 0, 0, 0 };
-            UI.Log(String.Format("{0} {1}", loc[0], loc[1]));
             byte[,] temp_planet = Program.world;
             surroundings[0] = temp_planet[loc[0] - 1, loc[1]];
             surroundings[1] = temp_planet[loc[0], loc[1] + 1];
             surroundings[2] = temp_planet[loc[0] + 1, loc[1]];
             surroundings[3] = temp_planet[loc[0], loc[1] - 1];
-            for (int c = 0; c < surroundings.Length; c++ )
-            {
-                UI.Log(surroundings[c].ToString());
-            }
             return surroundings;
         }
 
@@ -34,38 +29,59 @@ namespace WaterRising
                 if (action_group == 0)
                 {
                     // climb
-                    int[] res = IsPlayerAdjacent(1);
-                    UI.Log(String.Format("{0} {1}, {2} {3}", Player.pos[0], Player.pos[1], res[0], res[1]));
+                    if (IsPlayerAdjacent(1))
+                    {
+                        Player.pos = GetAdjacentBlock(1);
+                        UI.Log("You climb the nearest mountain");
+                    }
                 }
             }
             return "";
         }
 
-        public static int[] IsPlayerAdjacent(int block)
+        public static bool IsPlayerAdjacent(int block)
+        {
+            bool next_to = false;
+            byte[] surr = GetSurround(Player.pos);
+            foreach (byte adj_block in surr)
+            {
+                if (adj_block == block)
+                {
+                    next_to = true;
+                }
+            }
+            return next_to;
+        }
+
+        public static int[] GetAdjacentBlock(int block)
         {
             int[] ppos = Player.pos;
             byte[] surr = GetSurround(ppos);
             int[] return_list = {-1, -1};
-            for (int count = 0; count < surr.Length; count++ )
+            for (int count = 1; count < surr.Length; count++ )
             {
-                if (surr[count] == block)
+                if (surr[count - 1] == block)
                 {
                     // THIS IS AWFUL STYLE I HATE THIS WHOLE FUNCTION
                     if (count == 1)
                     {
-                        ppos[0] += 1;
+                        ppos[0] -= 1;
+                        break;
                     }
                     else if (count == 2)
                     {
                         ppos[1] += 1;
+                        break;
                     }
                     else if (count == 3)
                     {
                         ppos[0] -= 1;
+                        break;
                     }
                     else if (count == 4)
                     {
-                        ppos[1] -= 1;
+                        ppos[1] += 1;
+                        break;
                     }
                 }
             }
